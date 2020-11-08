d7a616ae31010e2d92334f347f8e0f97eca422c0
Jjh <jpsfebk@gmail.com>
2020-11-09 01:15:27 +0800

    CRUD 第一版

---
+++ YCwebTest/YCwebTest/Data/YCwebTestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace YCwebTest.Data
+{
+    public class YCwebTestContext : DbContext
+    {
+        public YCwebTestContext() : base("name=YCwebTestContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<YCwebTest.Models.Customer> Customers { get; set; }
+    }
+}
